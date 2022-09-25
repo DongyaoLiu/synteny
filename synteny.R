6a21e74e347b6b6a6da346e8372c67a40c2c501b
DongyaoLiu <1404805140@qq.com>
2022-09-25 11:42:11 +0800

    test synteny Rscript

--- synteny.R
+++ synteny.R
@@ -2,9 +2,9 @@
 library(gggenomes)
 
 ##read python script prepared data
-ava = read.table(file = "./example2/ele_X_1050000_1150000.ava")
+ava = read.table(file = "./example2/ele_X_1050000_1150000.nogap.ava")
 seq = read.table(file = "./example2/ele_X_1050000_1150000.seq", header = T)
-gene = read.table(file = "./example2/ele_X_1050000_1150000.ana")
+gene = read.table(file = "./example2/ele_X_1050000_1150000.nogap.ana")
 
 ##munipulation gene dataset
 colnames(gene) = c("species", "scaff", "type", "start", "end", "strand","name")
@@ -37,17 +37,35 @@ sub_ava = ava[which(ava$seq_id2 %in% by_species_single_seq$seq_id), ]
 sub_ava_dup = sub_ava %>% filter(seq_id2 == seq_id)
 sub_ava_no_dup = sub_ava %>% filter(seq_id2 != seq_id)
 
+###gene dataset test
+test_gene = sub_gene %>% group_by(species) %>% arrange(species, scaff, start) %>%
+  filter(between(row_number(), 1,10))
+gggenomes(genes = test_gene) + geom_gene() #it works, but not beautiful
+
+###Because geom_gene don't realize type "gene"
+###As the CDS to much when window size is large, so we do some munipulation on gene_set
+munipu_gene = sub_gene %>% filter(type == "gene") 
+munipu_gene$type = rep("CDS", length(munipu_gene))
+gggenomes(genes = munipu_gene) + geom_gene() #it works, but not beautiful
+## Because the compact genomes
+
+
 ## start plot
-p <- gggenomes(seq = by_species_single_seq, genes = sub_gene, links = sub_ava_no_dup) +
+p <- gggenomes(seq = by_species_single_seq, genes = munipu_gene, links = sub_ava_no_dup) +
   geom_seq() +
   geom_bin_label() +
-  geom_link() +
-  geom_gene(aes(fill = name)) +
-  geom_gene_tag(aes(label=name), nudge_y=0.1, check_overlap = TRUE) 
+  geom_link()
+  
 
-p
-ggsave(filename = "ele_X_1050000_1150000.synteny.pdf", device = "pdf",dpi = 300,
-       limitsize = F)
+
+
+
+ggsave(filename = "ele_X_1050000_1150000.synteny.png",plot = p, device = "png",dpi = 300,
+       limitsize = T)
+png("ele_X_1050000_1150000.synteny.png")
+print(p)
+dev.off()
+
 
 data(package="gggenomes")
 
